a52d44f35ae746989e5a99da64af70efb3d49886
Csaba Toth <csaba.toth.work@protonmail.com>
2021-06-22 10:56:07 +0200

    Initial commit

---
+++ src/com/luv2code/hibernate/demo/CreateDemo.java
@@ -0,0 +1,51 @@
+package com.luv2code.hibernate.demo;
+
+import com.luv2code.demo.entity.Instructor;
+import com.luv2code.demo.entity.InstructorDetail;
+import com.luv2code.demo.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateDemo {
+
+    public static void main(String[] args) {
+
+
+        // create session factory
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+
+
+            Instructor tempInstructor = new Instructor("Madhu", "Patel", "madhu@luv2code.com");
+
+            InstructorDetail tempInstructorDetails = new InstructorDetail("http://www.luv2code.com/youtube",
+                    "guitar");
+
+            // associate the objects
+            tempInstructor.setInstructorDetail(tempInstructorDetails);
+
+            // start a transaction
+            session.beginTransaction();
+
+            // save the instructor
+            // this will also save the details object, cuz of CascadeType.ALL
+            System.out.println("Saving instructor: " + tempInstructor);
+            session.save(tempInstructor);
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        } finally {
+            factory.close();
+        }
+    }
+}
